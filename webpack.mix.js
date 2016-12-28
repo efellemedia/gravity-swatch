d2af7afa91dd8999ddde0e1585b12460dfe062bc
Shea Lewis <shea.lewis89@gmail.com>
2016-12-27 18:56:24 -0800

    Initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,28 @@
+let mix = require('laravel-mix').mix;
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+mix.js('./src/gravity.js', 'dist')
+    .sass('./src/gravity.scss', 'dist');
+
+// Full API
+// mix.js(src, output);
+// mix.extract(vendorLibs);
+// mix.sass(src, output);
+// mix.less(src, output);
+// mix.combine(files, destination);
+// mix.copy(from, to);
+// mix.minify(file);
+// mix.sourceMaps(); // Enable sourcemaps
+// mix.version(); // Enable versioning.
+// mix.disableNotifications();
+// mix.setCacheDirectory('some/folder');
