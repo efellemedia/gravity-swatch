1cd7ae8f7832e7f723aaaeda084b92eae0a4b71f
Shea Lewis <shea.lewis89@gmail.com>
2017-01-01 14:04:08 -0800

    tweaks

--- src/js/gravity.js
+++ src/js/gravity.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import Quill from 'quill';
 import VueEvents from './events.js';
 import App from './components/App.vue';
 import UISidebar from './components/UISidebar.vue';
